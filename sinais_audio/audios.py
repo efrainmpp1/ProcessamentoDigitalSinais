cc03f8ee9cc30bc989423ed0fe9e50617b79fda9
efrainmpp1 <efrainmpp@hotmail.com>
2022-04-07 15:58:39 -0300

    Adicionando Subplot

--- sinais_audio/audios.py
+++ sinais_audio/audios.py
@@ -11,17 +11,18 @@ tempo_segundos_fim_sinal = len(sinal)*periodo
 
 tempo = np.arange(0,tempo_segundos_fim_sinal,periodo) # criando vetor tempo
 
-# plotando o sinal
-plt.title("Sinal de Audio")
-plt.plot(tempo,sinal)
-plt.show()
-
 sinal_invertido = np.flip(sinal) # invertendo sinal de audio
 
 # Gerando um arquivo de audio com o sinal invertido
 sf.write('audio_invertido.wav', sinal_invertido ,taxa_amostragem) 
 
 
-
-
+# plotando os dois graficos
+figure, axis = plt.subplots(2,1)
+plt.subplots_adjust(wspace=0.25,hspace=0.5)
+axis[0].set_title("Sinal")
+axis[0].plot(tempo,sinal)
+axis[1].set_title("Sinal invertido")
+axis[1].plot(tempo,sinal_invertido)
+plt.show()
 
