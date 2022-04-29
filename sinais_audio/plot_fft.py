918e685065fe61ae8c4466f193921f8d0687a158
efrainmpp1 <efrainmpp@hotmail.com>
2022-04-29 00:47:45 -0300

    Simplificando código

--- sinais_audio/plot_fft.py
+++ sinais_audio/plot_fft.py
@@ -7,16 +7,8 @@ sinal , taxa_amostragem = sf.read('audio.wav') # você pode escolher o arquivo d
 
 # Trazendo nosso sinal para uma dimensão
 sinal = np.ndarray.flatten(sinal)
-periodo = 1/(2*taxa_amostragem)
 
-sinal_fft = np.fft.fft(sinal) # aplicando a transformada rapida de Fourier
-
-sinal_fft = sinal_fft.real # pegando apenas a parte real para trabalharmos
-
-frequencias = np.fft.fftfreq(len(sinal_fft),d=periodo) # montando nosso eixo das frequencias
-mascara = frequencias >= 0 #Condição de tirar a parte espelhada e só ficando com o quadrante positivo
-
-plt.plot(frequencias[mascara],sinal_fft[mascara])
+plt.magnitude_spectrum(sinal , Fs= 2*taxa_amostragem )
 plt.title("Sinal no Dominio Frequencia")
 plt.xlabel("Frequencia[Hz]")
 plt.ylabel("Amplitude")
